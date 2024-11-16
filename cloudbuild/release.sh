9690de9181a05989c3a9af65edf21ea0ff9a7e10
krsingh081206 <kumar.ritesh@hsc.com>
2024-11-16 06:41:17 +0000

    changes to support spring boot svc

--- cloudbuild/release.sh
+++ cloudbuild/release.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 dbimage=`jq -r '.staging | .db' /workspace/cloudbuild/$release/release.json`
 backendimage=`jq -r '.staging | .backend' /workspace/cloudbuild/$release/release.json`
+javabackendimage=`jq -r '.staging | .springbootsvc' /workspace/cloudbuild/$release/release.json`
 frontendimage=`jq -r '.staging | .frontend' /workspace/cloudbuild/$release/release.json`
 
 
 echo $dbimage
 echo $backendimage
+echo $javabackendimage
 echo $frontendimage
 
 
 # updating k8 yaml for staging
 # Yaml update for stg
 sed -i "s/image: backendvm/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$backendimage/g" /workspace/cloudbuild/techgig-cicd-demo-backend/k8s/staging/api-deployment.yaml
+sed -i "s/image: springbootbackend/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$javabackendimage/g" /workspace/cloudbuild/cicd-demo-backend-java/k8s/staging/api-deployment.yaml
 sed -i "s/image: frontendvm/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$frontendimage/g" /workspace/cloudbuild/techgig-cicd-demo-frontend/k8s/staging/fe-deployment.yaml
 sed -i "s/image: IMAGE/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$dbimage/g" /workspace/cloudbuild/techgig-cicd-demo-db/k8s/staging/db-deployment.yaml
 
 #Displaying yaml value for stg yaml
 cat /workspace/cloudbuild/techgig-cicd-demo-backend/k8s/staging/api-deployment.yaml
+cat /workspace/cloudbuild/cicd-demo-backend-java/k8s/staging/api-deployment.yaml
 cat /workspace/cloudbuild/techgig-cicd-demo-frontend/k8s/staging/fe-deployment.yaml
 cat /workspace/cloudbuild/techgig-cicd-demo-db/k8s/staging/db-deployment.yaml
