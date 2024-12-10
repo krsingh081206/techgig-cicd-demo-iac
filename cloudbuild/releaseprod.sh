cb7644204ea7d1d6d645f315fc99fe8aeb399d31
krsingh081206 <kumar.ritesh@hsc.com>
2024-12-10 17:19:18 +0000

    added support for prod pipeline

--- cloudbuild/releaseprod.sh
+++ cloudbuild/releaseprod.sh
@@ -13,13 +13,13 @@ echo $frontendimage
 
 # updating k8 yaml for production
 # Yaml update for stg
-sed -i "s/image: backendvm/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$backendimage/g" /workspace/cloudbuild/techgig-cicd-demo-backend/k8s/production/api-deployment.yaml
-sed -i "s/image: springbootbackend/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$javabackendimage/g" /workspace/cloudbuild/cicd-demo-backend-java/k8s/production/api-deployment.yaml
-sed -i "s/image: frontendvm/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$frontendimage/g" /workspace/cloudbuild/techgig-cicd-demo-frontend/k8s/production/fe-deployment.yaml
-sed -i "s/image: IMAGE/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$dbimage/g" /workspace/cloudbuild/techgig-cicd-demo-db/k8s/production/db-deployment.yaml
+sed -i "s/image: backendvm/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$backendimage/g" /workspace/cloudbuild/techgig-cicd-demo-backend/k8s/prod/api-deployment.yaml
+sed -i "s/image: springbootbackend/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$javabackendimage/g" /workspace/cloudbuild/cicd-demo-backend-java/k8s/prod/api-deployment.yaml
+sed -i "s/image: frontendvm/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$frontendimage/g" /workspace/cloudbuild/techgig-cicd-demo-frontend/k8s/prod/fe-deployment.yaml
+sed -i "s/image: IMAGE/image: us-east4-docker.pkg.dev\/$projectid\/techgig-cicd\/$dbimage/g" /workspace/cloudbuild/techgig-cicd-demo-db/k8s/prod/db-deployment.yaml
 
 #Displaying yaml value for stg yaml
-cat /workspace/cloudbuild/techgig-cicd-demo-backend/k8s/production/api-deployment.yaml
-cat /workspace/cloudbuild/cicd-demo-backend-java/k8s/production/api-deployment.yaml
-cat /workspace/cloudbuild/techgig-cicd-demo-frontend/k8s/production/fe-deployment.yaml
-cat /workspace/cloudbuild/techgig-cicd-demo-db/k8s/production/db-deployment.yaml
+cat /workspace/cloudbuild/techgig-cicd-demo-backend/k8s/prod/api-deployment.yaml
+cat /workspace/cloudbuild/cicd-demo-backend-java/k8s/prod/api-deployment.yaml
+cat /workspace/cloudbuild/techgig-cicd-demo-frontend/k8s/prod/fe-deployment.yaml
+cat /workspace/cloudbuild/techgig-cicd-demo-db/k8s/prod/db-deployment.yaml
