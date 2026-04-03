a52f4cecca443aa0043dae5b6f90aeda4bf8fd9f
ritesh <krsingh081206@gmail.com>
2026-04-04 00:10:44 +0530

    updated project name

--- cloudbuild/environments/staging/variables.tf
+++ cloudbuild/environments/staging/variables.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   description = "The ID of the project in which to provision resources."
   type        = string
-  default     = "rd-application-group"
+  default     = "gcp-poc-app-dev"
 }
 
 variable "network_name" {
